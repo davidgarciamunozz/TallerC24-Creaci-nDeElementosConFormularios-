d4c336d0effcea0439d7737f135d803dff52a862
DavidG <garxia0710@gmail.com>
2024-04-20 16:01:31 -0500

    upToDate

---
+++ utils.js
@@ -0,0 +1,12 @@
+export const tareas = [
+    {
+        "id": 1,
+        "nombre": "Enseñar en clase",
+        "terminada": true
+    },
+    {
+        "id": 2,
+        "nombre": "Sacar la basura",
+        "terminada": false
+    }
+];
